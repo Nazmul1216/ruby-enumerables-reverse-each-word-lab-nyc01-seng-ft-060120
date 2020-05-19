5d6e397853d5af470d08ffc8ea6b6b75e199bbbb
Nazmul Kabir <nazmul.kabir1216@gmail.com>
2020-05-19 00:50:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,14 @@
 def reverse_each_word(string)
   new_array=string.split(" ")
   new_array_2=[]
+  new_array_3=string.split(" ")
+  new_array_4=[]
   p new_array
   new_array.each do|string|
     new_array_2.push(string.reverse)
   end
   new_array_2.join(" ")
+end 
+p new_array_3
   
 end 
